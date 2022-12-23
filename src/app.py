553a1b39e334ed29c74a381813d65d8221390241
MJRod23 <m.rodriguez4260@gmail.com>
2022-12-23 02:22:09 +0000

    added

--- src/app.py
+++ src/app.py
@@ -9,7 +9,11 @@ from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
 from models import db, User
+import requests
+import json
 #from models import Person
+
+
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
@@ -36,14 +40,44 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
+@app.route('/people', methods=['GET'])
+def handle_people():
+    response = requests.get('https://swapi.dev/api/people')
+    return (response.json()["results"]), 200
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+@app.route('/planets', methods=['GET'])
+def handle_planet():
+    response = requests.get('https://swapi.dev/api/planets')
+    return (response.json()["results"]), 200
 
-    return jsonify(response_body), 200
+@app.route('/people/<people_id>', methods=['GET'])
+def handle_id(people_id):
+    response = requests.get('https://swapi.dev/api/people/' + people_id)
+    return (response.json()), 200
+
+@app.route('/planets/<planet_id>', methods=['GET'])
+def handle_planetid(planet_id):
+    response = requests.get('https://swapi.dev/api/planets/' + planet_id)
+    return (response.json()), 200
+
+@app.route('/users', methods=['GET'])
+def handle_users():
+    users_query=User.query.all()
+    all_users=list(map(lambda x:x.serialize(),users_query))
+    return all_users, 200
+
+@app.route('/favorite/planet/<planet_id>/<user_id>', methods=['POST'])
+def handle_favorite_planets(planet_id, user_id):
+    user1 = User.query.get(user_id)
+    if user1 is None:
+        user1 = User(id=user_id, favorite_planets=json.dumps([planet_id]), favorite_people=json.dumps([]))
+        db.session.add(user1)
+        db.session.commit()
+        return 'ok', 200
+
+    user1.favorite_planets.append(planet_id)
+    db.session.commit()
+    return 'ok', 200
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -1,19 +1,25 @@
 from flask_sqlalchemy import SQLAlchemy
-
+import json
 db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    favorite_planets = db.Column(db.String(200))
+    favorite_people = db.Column(db.String(200))
+
+    # email = db.Column(db.String(120), unique=True, nullable=False)
+    # password = db.Column(db.String(80), unique=False, nullable=False)
+
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<User %r>' % self.id
 
-    def serialize(self):
+    def serialize(self):  
         return {
             "id": self.id,
-            "email": self.email,
+            # "email": self.email,
+            "favorite_planets" : json.loads(self.favorite_planets),
+            "favorite_people" : json.loads(self.favorite_people)
+
             # do not serialize the password, its a security breach
         }
